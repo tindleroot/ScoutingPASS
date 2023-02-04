f223fb0a0d1531db1f46cb3f1fe9e30bafeb6a37
Tindleroot <tindleroot@gmail.com>
2023-02-03 19:46:07 -0800

    Uses my personal TBA auth key

--- resources/js/TBAInterface.js
+++ resources/js/TBAInterface.js
@@ -1,7 +1,7 @@
 // TBAInterface funcitons to pull data from TheBlueAlliance.com
 var teams = null;
 var schedule = null;
-var authKey = "uTHeEfPigDp9huQCpLNkWK7FBQIb01Qrzvt4MAjh9z2WQDkrsvNE77ch6bOPvPb6";
+var authKey = "0DAyI19O7g9ZOMe1BohkeEjrhxUjNLt1y595LvuL9d4O94igsaozCrAzA7RBlbrf";
 /**
  * Get list of teams in event
  *
