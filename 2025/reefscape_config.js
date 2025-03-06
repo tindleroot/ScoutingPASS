b0225540368d65e2c7133edd14c49ab528498e05
Tindleroot <tindleroot@gmail.com>
2025-03-06 10:41:10 -0800

    Remove auto scoring position box

--- 2025/reefscape_config.js
+++ 2025/reefscape_config.js
@@ -86,14 +86,6 @@ var config_data = `
       "code": "ac4",
       "type": "counter"
     },
-    { "name": "Auto Scoring Position",
-      "code": "asp",
-      "type": "clickable_image",
-      "filename": "2025/reef.png",
-      "dimensions": "6 6",
-      "allowableResponses": "1 2 3 4 5 6 7 8 9 10 11 12 13 14 17 18 19 20 23 24 25 26 27 28 29 30 31 32 33 34 35 36",
-      "shape": "circle 5 black red true"
-    },
     { "name": "Processor Score",
       "code": "aps",
       "type": "counter"
