2b8585143696a78013fc0c32c8f0ed13a1506bf0
Tindleroot <tindleroot@gmail.com>
2025-03-21 01:32:29 -0700

    Set event code for Auburn

--- 2025/reefscape_config.js
+++ 2025/reefscape_config.js
@@ -15,7 +15,7 @@ var config_data = `
     { "name": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": "2025wasam",
+      "defaultValue": "2025waahs",
       "required": "true"
     },
     { "name": "Match Level",
