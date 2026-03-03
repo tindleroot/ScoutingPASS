27c6ab6b20923f8b8a389aee11243ba10786f0e0
Tindleroot <tindleroot@gmail.com>
2026-03-03 13:36:20 -0800

    Set current event to week 0 for practice

--- 2026/rebuilt_config.js
+++ 2026/rebuilt_config.js
@@ -15,7 +15,7 @@ var config_data = `
     { "name": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": "2026ilch",
+      "defaultValue": "2026week0",
       "required": "true"
     },
     { "name": "Match Level",
