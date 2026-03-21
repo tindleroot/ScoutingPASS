57af464a55e2c853ad5d6d8dd825feff0e4ad826
Tindleroot <tindleroot@gmail.com>
2026-03-20 17:41:01 -0700

    Change default event code to sammamish

--- 2026/rebuilt_config.js
+++ 2026/rebuilt_config.js
@@ -15,7 +15,7 @@ var config_data = `
     { "name": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": "2026wabon",
+      "defaultValue": "2026wasam",
       "required": "true"
     },
     { "name": "Match Level",
